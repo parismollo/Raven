e9035adf5d9ad9045a7c01ecfe9e8b8d9712e9a9
Paris <parismollochristondis@gmail.com>
2020-01-09 19:57:04 +0100

    add widgets

--- logistic_regression.py
+++ logistic_regression.py
@@ -5,11 +5,15 @@ from sklearn import datasets
 
 st.sidebar.title('Logistic Regression')
 
-st.subheader('Logistic Regression class')
+st.subheader('Understanding Logistic Regression')
+st.write('Logistic regression is an algorithm used to classify observations to a discrete number of classes, 1 and 0, for example.')
+
+st.subheader('Linear Vs Logistic')
+st.write('Linear regression predictions **are continuous**, they will be used to predict  the score of a student, or the heigth of a person. In the other hand, Logistic regression predictions are discrete, they will assign a probability score related to some **discrete classification**, such as, pass or fail the exam.')
+
+# st.subheader('Building a Logistic Regression model')
 
 class LogisticRegression:
-    # Modify the learning rate option
-    # Modify number_of_iterations option
     def __init__(self, learning_rate=0.001, number_of_iterations=1000):
         self.learning_rate = learning_rate
         self.number_of_iterations = number_of_iterations
@@ -56,10 +60,11 @@ class LogisticRegression:
     def _sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
-st.subheader('Testing')
+st.subheader('Sklearn Breast Cancer Dataset - Model application')
+
 data_load_state = st.text('Loading data...')
 data = datasets.load_breast_cancer()
-data_load_state.text('Loading data... done!')
+data_load_state.text('Loading data... Done!')
 
 X, y = data.data, data.target
 
@@ -69,7 +74,11 @@ def accuracy(y_true, y_pred):
     accuracy = np.sum(y_true == y_pred) / len(y_true)
     return accuracy
 
-model = LogisticRegression(learning_rate=0.001, number_of_iterations=1000)
+number_of_iterations = st.slider('Choose the number of iterations', 0, 2000, 10)
+learning_rate = st.selectbox('Choose the learning rate', (0.001, 0.005, 0.01))
+
+model = LogisticRegression(learning_rate=learning_rate, number_of_iterations=number_of_iterations)
 model.fit(X_train, y_train)
+
 predictions = model.predict(X_test)
 st.write("Accuracy: ", accuracy(y_test, predictions))
