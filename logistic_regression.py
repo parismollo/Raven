f6c8002acc2d0d28e794fb50186512053b54c9cd
Paris <parismollochristondis@gmail.com>
2020-01-11 17:24:23 +0100

    add info

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,15 +3,29 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 
-st.sidebar.title('Logistic Regression')
+st.title('Logistic Regression')
 
-st.subheader('Understanding Logistic Regression')
+st.subheader('Understanding Logistic Regression :books:')
 st.write('Logistic regression is an algorithm used to classify observations to a discrete number of classes, 1 and 0, for example.')
+
+st.subheader('The Idea :bulb:')
+st.write('The goal is to predict whether the cancer is **benign** or **malignant** based on 10 real-valued features computed for each **cell nucleus**. To map predicted values to probabilities, we use the **sigmoid function**. The function maps any real value into another value between 0 (benign) and 1 (malignant).')
+
+st.subheader('The process')
+st.write('To be able to predict it, we will use a linear regression like this one: ')
+st.latex(r''' y = bias + W_0x_1 + W_1x_2 + ...''')
+st.write('This time however we will transform the output using the sigmoid function to return a probability value between 0 and 1.')
+st.latex(r''' S(y) = \frac {1} {(1 + e^{-y})}''')
 
 st.subheader('Linear Vs Logistic')
 st.write('Linear regression predictions **are continuous**, they will be used to predict  the score of a student, or the heigth of a person. In the other hand, Logistic regression predictions are discrete, they will assign a probability score related to some **discrete classification**, such as, pass or fail the exam.')
 
-# st.subheader('Building a Logistic Regression model')
+st.subheader('Optimizing our results :dart:')
+st.write(''' Now to find the best predictions we need to find the values of our parameters, the weights, and bias, which **will minimize the errors of our predictions**. To measure the errors of our predictions we need a **cost function**, in linear regression, we use the sum of squared errors and then we apply the gradient descent technique to look for the best set of parameters that will give us the smallest error. However, in this scenario, the SSR cost function doesn't work.  So we will use the **cross-entropy cost** function and apply the gradient descent.
+
+We want to optimize the cost function with respect to our parameters, weight, and bias.
+
+We will start somewhere, compute the derivative, update the parameters, **move towards the direction until we find the cost minimum**''')
 
 class LogisticRegression:
     def __init__(self, learning_rate=0.001, number_of_iterations=1000):
@@ -73,8 +87,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 def accuracy(y_true, y_pred):
     accuracy = np.sum(y_true == y_pred) / len(y_true)
     return accuracy
-
+st.markdown('_Iterations determine the number of times we will update our parameters and move towards another point in our curve, hopefully, towards the minimum_. **Increasing the number of iterations may rise the accuracy of our model** :smile:')
 number_of_iterations = st.slider('Choose the number of iterations', 0, 2000, 10)
+st.markdown('_The learning rate determines the distance in which we move between iterations._')
 learning_rate = st.selectbox('Choose the learning rate', (0.001, 0.005, 0.01))
 
 model = LogisticRegression(learning_rate=learning_rate, number_of_iterations=number_of_iterations)
